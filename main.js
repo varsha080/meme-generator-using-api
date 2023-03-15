ec022792bcdf13d30bd270386707adcc58bcbd31
Varsha Gupta <89845598+varsha080@users.noreply.github.com>
2023-03-15 12:14:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const btn = document.getElementById('gen-meme');
 const memeimg = document.getElementById('meme-img');
 const url = 'https://meme-api.com/gimme/';
 const subreddits = ["catmemes", "me_irl","dogmemes","wholesomememes"]
-
+alert('hyee')
 let getmeme= () =>{
     let randomsubred = subreddits[ Math.floor(Math.random()* 4)];
     fetch(url+randomsubred).then(resp => resp.json()).
