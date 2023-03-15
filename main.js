586161202cccd1f262dbb62b7222255e3772f087
Varsha Gupta <89845598+varsha080@users.noreply.github.com>
2023-03-15 11:53:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const btn = document.getElementById('gen-meme');
+const memeimg = document.getElementById('meme-img');
+const url = 'https://meme-api.com/gimme/';
+const subreddits = ["catmemes", "me_irl","dogmemes","wholesomememes"]
+
+let getmeme= () =>{
+    let randomsubred = subreddits[ Math.floor(Math.random()* 4)];
+    fetch(url+randomsubred).then(resp => resp.json()).
+    then((data) =>{
+        console.log(data);
+        memeimg.setAttribute("src",data.url);
+    })
+}
+
+btn.addEventListener("click", getmeme);
+getmeme();
